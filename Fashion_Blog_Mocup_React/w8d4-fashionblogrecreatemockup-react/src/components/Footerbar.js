0a92774f24086a3fcd5797cbf5f54a0826a10295
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-17 15:25:34 -0400

    minor change

---
+++ Fashion_Blog_Mocup_React/w8d4-fashionblogrecreatemockup-react/src/components/Footerbar.js
@@ -0,0 +1,20 @@
+export default function Footerbar(){
+    return <footer className="footerbar">
+      <div>
+      <ul>
+      <a href="/" className="footertab">Home</a>
+      
+      <a href="/" className="footertab">Women's</a>
+      
+      <a href="/" className="footertab">Men's</a>
+      <a href="/" className="footertab">On the Street</a>
+      <a href="/" className="footertab">The Catwalk</a>
+      <a href="/" className="footertab">AdWatch</a>
+      <a href="/" className="footertab">About</a>
+      <a href="/" className="footertab">Tips</a>
+      
+      </ul>
+      </div>
+     </footer>
+     }
+    
