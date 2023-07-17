f8b51fb3c6ed1f8c3b2d941810d4787df45d5290
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-17 15:25:54 -0400

    minor change

---
+++ Fashion_Blog_Mocup_React/w8d4-fashionblogrecreatemockup-react/src/components/Navbar.js
@@ -0,0 +1,22 @@
+export default function Navbar(){
+return <nav className="nav">
+  <div>
+  <ul>
+    
+  <a href="/" className="firsttab">Women's</a>
+  
+  <a href="/" className="firsttab">Men's</a>
+  <a href="/" className="firsttab">On the Street</a>
+  <a href="/" className="firsttab">The Catwalk</a>
+  <a href="/" className="firsttab">AdWatch</a>
+  <a href="/" className="firsttab">About</a>
+  </ul>
+  </div>
+  </nav>
+
+
+
+
+}
+
+
