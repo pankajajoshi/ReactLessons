f25c5be9c83b47109ba2cb1d1bc3b9b61d9fe906
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-17 15:20:58 -0400

    added article content to blog

---
+++ Fashion_Blog_Mocup_React/w8d4-fashionblogrecreatemockup-react/src/components/Article.js
@@ -0,0 +1,18 @@
+
+
+export default function Article() {
+    return (
+      <Article className="articlecontent">
+        <div>
+          
+            <div className="datecontent">11/12/20</div>
+         
+          <div
+             className="titlecontent">On the Street in Brooklyn</div>
+          
+          
+       
+        </div>
+      </Article>
+    );
+  }
