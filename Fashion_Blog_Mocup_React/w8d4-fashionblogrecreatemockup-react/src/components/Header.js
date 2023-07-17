0ef42e5cef63cf043729113d253447945ce26c42
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-16 21:23:13 -0400

    Adding Header to mock

---
+++ Fashion_Blog_Mocup_React/w8d4-fashionblogrecreatemockup-react/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div >
+      <span className="mainheading" >Sartre's List</span> 
+     
+      <div ><span className="subheading">Better-Dressed People</span></div>
+    </div>
+  )
+}
+
+export default Header;
