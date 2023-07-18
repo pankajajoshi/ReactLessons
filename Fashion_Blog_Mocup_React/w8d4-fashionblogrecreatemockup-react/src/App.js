71c77074f84b7872de2b8043a15140260ff97758
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-17 21:58:25 -0400

    extra paragraph added

--- Fashion_Blog_Mocup_React/w8d4-fashionblogrecreatemockup-react/src/App.js
+++ Fashion_Blog_Mocup_React/w8d4-fashionblogrecreatemockup-react/src/App.js
@@ -28,27 +28,52 @@ function App() {
             <div className='img2content'>
               <img src={img1} alt="img1" /></div>
               <div className="divfirstpara">
-              <span className="firstpara" >
+              <p className="firstpara" >
                 Cary ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                  Excepteur sint occaecat cupidatat 
-                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-                </div>
-             
-              <div className='img2content'>
-              <img src={img2} alt="img2" /></div>
-              <div className="divsecondpara">
-              <span className="secondpara">Selfies ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                  Excepteur sint occaecat cupidatat 
-                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborumconsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat 
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p className='continue'>Continues....</p>
+                </div>
+              <hr></hr>
+                <div className="datecontent">11/11/20</div>
+                <div
+             className="titlecontent">Vintage in Vogue</div>
+              <div className='img2content'>
+              <img src={img2} alt="img2" /></div>
+              <div className="divsecondpara">
+              <p className="secondpara">Selfies ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat 
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat 
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborumconsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat 
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p className='continue'>Continues....</p>
+                
               </div>
+              <hr></hr>
         </div>
       </article>
       <Footerbar />
-                <div className="copyrightfooter">©2013 Valet Industries,Inc</div> 
+                
     </div> 
   );
 }
